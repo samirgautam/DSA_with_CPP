0f786b0c36eeeb733789e99b33304024711ca837
samir gautam <samirgautam003@gmail.com>
2022-04-13 19:00:51 +0545

    day3

--- 11.4postfixEvaluation.cpp
+++ 11.4postfixEvaluation.cpp
@@ -2,20 +2,20 @@
 #include<stack>
 #include<math.h>
 using namespace std;
-    int prefixEvaluation(string s)
-    {
+int postfixEvaluation(string s)
+{
     stack<int>st;
-    for(int i = s.length()-1;i>=0;i--)
+    for (int i = 0; i <= s.length(); i++)
     {
-        if(s[i] >= '0' && s[i] <= '9'){
+        if(s[i] >= '0' && s[i] <= '9')
+        {
             st.push(s[i] - '0');
         }
         else{
-            int op1 = st.top();
-            st.pop();
             int op2 = st.top();
             st.pop();
-
+            int op1 = st.top();
+            st.pop();
             switch (s[i])
             {
             case '+':
@@ -34,13 +34,13 @@ using namespace std;
                 st.push(pow(op1,op2));
                 break;
             }
+
         }
     }
-    return st.top();
-    }
+    return st.top();  
+}
+
 int main(){
-
-    cout<<prefixEvaluation("-+7*4520");
+    cout<<postfixEvaluation("46+2/5*7+")<<endl;
     return 0;
-
 }
